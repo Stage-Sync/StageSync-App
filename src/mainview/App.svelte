08c8fb25266716a639c7a724702656cab67e5245
DodoSeal <maxhmcd08@gmail.com>
2026-06-29 22:42:16 -0600

    Make test message

--- src/mainview/App.svelte
+++ src/mainview/App.svelte
@@ -1,4 +1,4 @@
 <div class="bg-slate-950 w-screen h-screen items-center place-content-center">
   <p class="text-center text-white font-bold text-[2rem]">Welcome to StageSync!</p>
-  <p class="text-center text-white font-thin text-[1.25rem]">[now time to make the app actually be an app...]</p>
+  <p class="text-center text-white font-thin text-[1.25rem]">This is a test release of StageSync to make sure GitHub Actions are working!</p>
 </div>
